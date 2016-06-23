87dedd18c13abe7f66a6dfd5d059fe91ebb0e28c
xinghao <lylor309@gmail.com>
2016-06-23 23:08:13 +1000

    fix start script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 #java -X8192msm -Xmx12288m -Dsolr.solr.home=/SOLR -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
-java -Dsolr.data.dir=/SOLR/data -Djava.util.logging.config.file=/SOLR/SolrServer/current/solr/pfparty/conf/logging.properties -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
+java -Djetty.home=/SOLR/SolrServer/current -Dsolr.solr.home=/SOLR/SolrServer/current/solr -Dsolr.data.dir=/SOLR/data -Djava.util.logging.config.file=/SOLR/SolrServer/current/solr/pfparty/conf/logging.properties -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
 #java -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar start.jar
+#java -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -Dsolr.data.dir=/Users/xinghao/Documents/rails4/solr-tracenames/data -jar start.jar
